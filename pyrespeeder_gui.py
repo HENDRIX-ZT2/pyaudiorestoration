445aaf20c1a02cff9433705b359aeadf7bae9ec1
HENDRIX-ZT2 <arnfinn@spholderer.de>
2018-07-16 16:14:34 +0200

    Reuse Pieces & Frustum Culling
    Spectrum pieces are only created if new ones are required, otherwise updated.
    Pieces out of the current view range are not drawn; this may or may not give a performance increase.

--- pyrespeeder_gui.py
+++ pyrespeeder_gui.py
@@ -9,6 +9,7 @@
 import os
 import numpy as np
 from vispy import scene, gloo, visuals, color, util, io
+from vispy.geometry import Rect
 import soundfile as sf
 from PyQt5 import QtGui, QtCore, QtWidgets
 
@@ -45,35 +46,44 @@ class Spectrum():
 		self.parent = parent
 		self.MAX_TEXTURE_SIZE = gloo.gl.glGetParameter(gloo.gl.GL_MAX_TEXTURE_SIZE)
 		self.mel_transform = vispy_ext.MelTransform()
+		self.empty = np.zeros((1,1), dtype="float32")
 	
 	def update_data(self, imdata, hop, sr):
 		num_bins, num_ffts = imdata.shape
-		#TODO: only delete / add visuals when required due to changing data, not always
-		print("removing old pieces")
-		for i in reversed(range(0, len(self.pieces))):
+		#determine how many pieces we will create
+		num_pieces_new = ((num_ffts-1)//self.MAX_TEXTURE_SIZE +1) * ((num_bins-1)//self.MAX_TEXTURE_SIZE+1)
+		num_pieces_old = len(self.pieces)
+		#we have too many pieces and need to discard some
+		for i in reversed(range(num_pieces_new, num_pieces_old)):
 			self.pieces[i].parent = None
 			self.pieces.pop(i)
-		#this seems to be correct, so could be used to decimate the old pieces
-		num_pieces_new = num_ffts//self.MAX_TEXTURE_SIZE + 1
-		#spectra may only be of a certain size, so split them
-		for x in range(0, num_ffts, self.MAX_TEXTURE_SIZE):
-			for y in range(0, num_bins, self.MAX_TEXTURE_SIZE):
-				imdata_piece = imdata[y:y+self.MAX_TEXTURE_SIZE, x:x+self.MAX_TEXTURE_SIZE]
-				num_piece_bins, num_piece_ffts = imdata_piece.shape
-				if num_piece_bins > 1 and num_piece_ffts > 1:
-					#do the dB conversion here because the tracers don't like it
-					self.pieces.append(SpectrumPiece(20 * np.log10(imdata_piece), parent=self.parent.scene))
-					
-					#to get correct heights for subsequent pieces, the start y has to be added in mel space
-					height_Hz_in = int(num_piece_bins/num_bins*sr/2)
-					ystart_Hz = (y/num_bins*sr/2)
-					height_Hz_corrected = to_Hz(to_mel(height_Hz_in + ystart_Hz) - to_mel(ystart_Hz))
-					
-					self.pieces[-1].set_size((num_piece_ffts*hop/sr, height_Hz_corrected))
+			# print("removing",i)
+		#add new pieces
+		for i in range(num_pieces_old, num_pieces_new):
+			self.pieces.append(SpectrumPiece(self.empty, parent=self.parent.scene))
+			# print("adding",i)
 
-					#add this piece's offset with STT
-					self.pieces[-1].transform = visuals.transforms.STTransform( translate=(x * hop / sr, to_mel(ystart_Hz))) * self.mel_transform
-		
+		#spectra may only be of a certain size, so split them
+		for i, (x, y) in enumerate([(x, y) for x in range(0, num_ffts, self.MAX_TEXTURE_SIZE) for y in range(0, num_bins, self.MAX_TEXTURE_SIZE)]):
+			imdata_piece = imdata[y:y+self.MAX_TEXTURE_SIZE, x:x+self.MAX_TEXTURE_SIZE]
+			num_piece_bins, num_piece_ffts = imdata_piece.shape
+			
+			#to get correct heights for subsequent pieces, the start y has to be added in mel space
+			height_Hz_in = int(num_piece_bins/num_bins*sr/2)
+			ystart_Hz = (y/num_bins*sr/2)
+			height_Hz_corrected = to_Hz(to_mel(height_Hz_in + ystart_Hz) - to_mel(ystart_Hz))
+			x_start = x * hop / sr
+			x_len = num_piece_ffts*hop/sr
+			
+			#do the dB conversion here because the tracers don't like it
+			self.pieces[i].tex.set_data(20 * np.log10(imdata_piece))
+			self.pieces[i].set_size((x_len, height_Hz_corrected))
+			#add this piece's offset with STT
+			self.pieces[i].transform = visuals.transforms.STTransform( translate=(x_start, to_mel(ystart_Hz))) * self.mel_transform
+			self.pieces[i].bb.left = x_start
+			self.pieces[i].bb.right = x_start+x_len
+			self.pieces[i].update()
+	
 	def set_clims(self, vmin, vmax):
 		for image in self.pieces:
 			image.set_clims(vmin, vmax)
@@ -81,18 +91,31 @@ class Spectrum():
 	def set_cmap(self, colormap):
 		for image in self.pieces:
 			image.set_cmap(colormap)
+			
+	def update_frustum(self, event):
+		for image in self.pieces:
+			#X frustum
+			if image.bb.left > self.parent.camera.rect.right or image.bb.right < self.parent.camera.rect.left:
+				image.hide()
+			else:
+				image.show()
+				
 
 class SpectrumPiece(scene.Image):
 	"""
 	The visualization of one part of the whole spectrogram.
 	"""
 	def __init__(self, texdata, parent):
+		self._parent2 = parent
 		#just set a dummy value
 		self._shape = (10.0, 22500)
 		self.get_data = visuals.shaders.Function(norm_luminance)
+		self.tex = gloo.Texture2D(texdata, format='luminance', internalformat='r32f', interpolation="linear")
 		self.get_data['vmin'] = -80
 		self.get_data['vmax'] = -40
-		self.get_data['texture'] = gloo.Texture2D(texdata, format='luminance', internalformat='r32f', interpolation="linear")
+		self.get_data['texture'] = self.tex
+		
+		self.bb = Rect((0, 0, 1, 1))
 
 		scene.Image.__init__(self, method='subdivide', grid=(1000,1), parent=parent)
 		
@@ -130,7 +153,13 @@ class SpectrumPiece(scene.Image):
 
 		if view._need_method_update:
 			self._update_method(view)
-
+			
+	def hide(self):
+		self.parent = None
+		
+	def show(self):
+		self.parent = self._parent2
+		
 def to_mel(val):
 	### just to set the image size correctly	
 	return np.log(val / 700 + 1) * 1127
@@ -985,6 +1014,7 @@ class Canvas(scene.SceneCanvas):
 		self.spec_xaxis.link_view(self.spec_view)
 		self.spec_yaxis.link_view(self.spec_view)
 		
+			
 		self.lines = []
 		self.regs = []
 		self.fft_storage = {}
@@ -992,6 +1022,10 @@ class Canvas(scene.SceneCanvas):
 		self.master_speed = MasterSpeedLine(self)
 		self.master_reg_speed = MasterRegLine(self)
 		self.spectrum = Spectrum(self.spec_view)
+		
+		#nb. this is a vispy.util.event.EventEmitter object
+		#can this be linked somewhere to the camera? base_camera connects a few events, too
+		self.spec_view.transforms.changed.connect(self.spectrum.update_frustum)
 		
 		self.freeze()
 		
@@ -1116,10 +1150,10 @@ class Canvas(scene.SceneCanvas):
 				s, ms = divmod(s*1000, 1000)
 				h, m = divmod(m, 60)
 				self.props.inspector_l.setText("\n    % 7.1f Hz\n%d:%02d:%02d:%03d h:m:s:ms" % (f, h, m, s, ms))
-			
+				
 	def on_mouse_release(self, event):
 		#coords of the click on the vispy canvas
-		if self.filename and len(event.trail()) and event.button == 1 and "Control" in event.modifiers:
+		if self.filename and (event.trail() is not None) and event.button == 1 and "Control" in event.modifiers:
 			last_click = event.trail()[0]
 			click = event.pos
 			if last_click is not None:
@@ -1156,7 +1190,8 @@ class Canvas(scene.SceneCanvas):
 							trace.set_offset(a[1], b[1])
 					self.master_speed.update()
 					self.master_reg_speed.update()
-							
+
+
 	def get_closest_line(self, click):
 		if click is not None:
 			#first, check in speed view

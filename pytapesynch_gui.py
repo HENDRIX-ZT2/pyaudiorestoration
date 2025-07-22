b0c7a0da7390c7ca11ec01a0f44a4fa6b901fa6b
HENDRIX-ZT2 <HENDRIX-ZT2@users.noreply.github.com>
2025-07-22 20:08:57 +0200

    fix pytapesynch_gui.py

--- pytapesynch_gui.py
+++ pytapesynch_gui.py
@@ -62,9 +62,9 @@ class Canvas(spectrum.SpectrumCanvas):
 		self.parent.props.filters_widget.bands_changed.connect(self.lag_line.update_bands)
 		self.parent.props.tracing_widget.setVisible(False)
 		self.parent.props.dropout_widget.setVisible(False)
-		self.freeze()
 		self.parent.props.alignment_widget.smoothing_s.valueChanged.connect(self.update_smoothing)
-		self.props.display_widget.fft_zeropad = 1
+		self.parent.props.display_widget.fft_zeropad = 1
+		self.freeze()
 
 	@property
 	def lags(self):
